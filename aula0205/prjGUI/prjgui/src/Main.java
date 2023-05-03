10b44e13936dcf8bf634ebc2063589c5a75f54a5
Maromo <professormoraes@gmail.com>
2023-05-02 22:11:10 -0300

    Aula do dia 02 de maio

--- aula0205/prjGUI/prjgui/src/Main.java
+++ aula0205/prjGUI/prjgui/src/Main.java
@@ -1,8 +1,15 @@
 import view.CaixaView;
+import view.Caixa_View;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        CaixaView tela = new CaixaView();
+       // CaixaView tela = new CaixaView();
+       // tela.setVisible(true);
+        JFrame tela = new JFrame();
+        tela.setContentPane(new Caixa_View().getPainelPrincipal());
         tela.setVisible(true);
+        tela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
--- aula0205/prjGUI/prjgui/src/view/CaixaView.java
+++ aula0205/prjGUI/prjgui/src/view/CaixaView.java
@@ -1,18 +1,27 @@
 package view;
 
+import model.Caixa;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class CaixaView extends JFrame {
+public class CaixaView extends JFrame implements WindowListener, ActionListener {
     //atributos da janela
     protected Dimension dTextField, dLabel, dTextArea, dButton, dFrame;
     protected Label lblValor, lblSaldo;
     protected TextField txtValor, txtSaldo;
     protected Button cmdEntrada, cmdRetirada, cmdConsulta, cmdSair;
     protected TextArea txtMsg;
+    private Caixa caixa;
 
     //método construtor
     public CaixaView(){
+        //instanciei o objeto caixa
+        caixa = new Caixa();
         //definir os tamanhos
         dFrame = new Dimension(400,400);
         dLabel = new Dimension(40,20);
@@ -47,26 +56,90 @@ public class CaixaView extends JFrame {
         cmdEntrada = new Button("Entrada");
         cmdEntrada.setSize(dButton);
         cmdEntrada.setLocation(25, 150);
+        cmdEntrada.addActionListener(this);
         this.add(cmdEntrada);
 
         cmdRetirada = new Button("Retirada");
         cmdRetirada.setSize(dButton);
         cmdRetirada.setLocation(180, 150);
+        cmdRetirada.addActionListener(this);
         this.add(cmdRetirada);
 
         cmdConsulta = new Button("Consulta");
         cmdConsulta.setSize(dButton);
         cmdConsulta.setLocation(25, 185);
+        cmdConsulta.addActionListener(this);
         this.add(cmdConsulta);
 
         cmdSair = new Button("Sair");
         cmdSair.setSize(dButton);
         cmdSair.setLocation(180, 185);
+        cmdSair.addActionListener(this);
         this.add(cmdSair);
 
         txtMsg = new TextArea(null);
         txtMsg.setSize(dTextArea);
         txtMsg.setLocation(25, 220);
         this.add(txtMsg);
+
+        this.addWindowListener(this);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        JOptionPane.showMessageDialog(null,
+                "Saindo com cuidado");
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (cmdSair.equals(source)) {
+            System.exit(0);
+        } else if (cmdEntrada.equals(source)) {
+            double valor = Double.parseDouble(txtValor.getText());
+            caixa.depositar(valor);
+            txtMsg.append("Depósito de " + valor + " feito \n");
+
+        } else if (cmdRetirada.equals(source)) {
+            double valor = Double.parseDouble(txtValor.getText());
+            caixa.sacar(valor);
+            txtMsg.append("Saque de " + valor + " feito \n");
+        } else if (cmdConsulta.equals(source)) {
+            txtSaldo.setText(Double.toString(caixa.getSaldo()));
+            txtMsg.append("Saldo consultado em tela \n");
+        } else {
+            System.out.println("Opcao Invalida");
+        }
     }
 }
